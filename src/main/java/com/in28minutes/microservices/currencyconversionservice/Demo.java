e4ffe6f2a16252c5db03926f7674a3a85055944d
gourav <gk97723747@gmail.com>
2021-11-21 16:38:24 +0530

    Add message for MOM and brother.

--- src/main/java/com/in28minutes/microservices/currencyconversionservice/Demo.java
+++ src/main/java/com/in28minutes/microservices/currencyconversionservice/Demo.java
@@ -4,6 +4,8 @@ public class Demo {
 	
 	public void display() {
 		System.out.println("Hello father, Glad to meet you :->");
+		System.out.println("Hi MOM, Glad to meet you :->");
+		System.out.println("Bye Brother, Glad to meet you :-<");
 	}
 
 }
